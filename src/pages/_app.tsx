0d859caa15f4405a52eb6126425090a69c0e51ba
Andre H <andrehadiantolesmana@gmail.com>
2022-07-28 17:24:33 +0800

    build: uses chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import "@/styles/globals.scss";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
 
 import { CoreLayout } from "@/common/components/CoreLayout";
 import { PageHead } from "@/common/components/PageHead";
+import { ChakraFonts } from "@/common/components/CustomFont";
+
+import theme from "@/theme";
 
 export const App = ({ Component, pageProps }) => {
   const Layout = Component.layout ? Component.layout : CoreLayout;
   return (
-    <>
+    <ChakraProvider theme={theme}>
+      <ChakraFonts />
       <Head>
         <meta
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
@@ -19,7 +24,7 @@ export const App = ({ Component, pageProps }) => {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </ChakraProvider>
   );
 };
 
