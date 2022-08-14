9dc6c9cfe506425a9e15af504f935c2b6562cc57
Andre H <andrehadiantolesmana@gmail.com>
2022-08-14 23:34:26 +0800

    build: update eslint

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,11 +2,11 @@ import { extendTheme } from "@chakra-ui/react";
 
 import { breakpoints } from "./breakpoints";
 import { colors } from "./colors";
+import { Button } from "./components/button";
+import { Heading } from "./components/heading";
+import { Text } from "./components/text";
 import { config } from "./config";
 import { fonts } from "./fonts";
-import { Button } from "./components/button";
-import { Text } from "./components/text";
-import { Heading } from "./components/heading";
 
 const theme = extendTheme({
   components: {
