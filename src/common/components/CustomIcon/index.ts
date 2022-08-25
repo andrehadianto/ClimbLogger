beabea44477e36c173621eb799a497fb86ebec1c
Andre H <andrehadiantolesmana@gmail.com>
2022-08-25 09:05:34 +0800

    feat: add new icons

--- src/common/components/CustomIcon/index.ts
+++ src/common/components/CustomIcon/index.ts
@@ -2,3 +2,6 @@ export * from "./CrossIcon";
 export * from "./CustomIcon";
 export * from "./EyeClosedIcon";
 export * from "./EyeOpenIcon";
+export * from "./ListLeftIcon";
+export * from "./PlusIcon";
+export * from "./UserIcon";
