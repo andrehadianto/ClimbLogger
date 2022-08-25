061ee56ddc50429ac28061cf5d852de1a52a4de0
Andre H <andrehadiantolesmana@gmail.com>
2022-08-25 13:38:21 +0800

    chore: add homeIcon instead

--- src/common/components/CustomIcon/index.ts
+++ src/common/components/CustomIcon/index.ts
@@ -2,6 +2,6 @@ export * from "./CrossIcon";
 export * from "./CustomIcon";
 export * from "./EyeClosedIcon";
 export * from "./EyeOpenIcon";
-export * from "./ListLeftIcon";
+export * from "./HomeIcon";
 export * from "./PlusIcon";
 export * from "./UserIcon";
