2e451c4d3690a652187a04023aa9e92b33850823
Andre H <andrehadiantolesmana@gmail.com>
2022-09-06 23:04:25 +0800

    feat: add filter Icon

--- src/common/components/CustomIcon/index.ts
+++ src/common/components/CustomIcon/index.ts
@@ -2,6 +2,7 @@ export * from "./CrossIcon";
 export * from "./CustomIcon";
 export * from "./EyeClosedIcon";
 export * from "./EyeOpenIcon";
+export * from "./FIlterIcon";
 export * from "./HomeIcon";
 export * from "./PlusIcon";
 export * from "./UserIcon";
