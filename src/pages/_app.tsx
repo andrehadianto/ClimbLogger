b17ab6721453b6816096da8416e55ff760b0a1c9
Andre H <andrehadiantolesmana@gmail.com>
2023-02-25 03:27:55 +0800

    feat: add react suspense and lazy loading for better webpack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-import { CoreLayout } from "@/common/components/CoreLayout";
 import { ChakraFonts } from "@/common/components/CustomFont";
+import { Loading } from "@/common/components/Loading";
 import { PageHead } from "@/common/components/PageHead";
 import { ProtectedLayout } from "@/common/components/ProtectedLayout";
 
@@ -16,28 +16,37 @@ import theme from "@/theme";
 import "@/styles/globals.scss";
 import "firebaseui/dist/firebaseui.css";
 
+const ChakraProvider = lazy(
+  async () =>
+    await import("@chakra-ui/react").then((mod) => {
+      return { default: mod.ChakraProvider };
+    })
+);
+
 export const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   const Layout = Component.layout ? Component.layout : ProtectedLayout;
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ChakraProvider theme={theme}>
-          <ChakraFonts />
-          <Head>
-            <meta
-              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-              name="viewport"
-            />
-            <PageHead />
-          </Head>
-          <CreateViewContextProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </CreateViewContextProvider>
-        </ChakraProvider>
-      </PersistGate>
-    </Provider>
+    <Suspense fallback={<Loading />}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ChakraProvider theme={theme}>
+            <ChakraFonts />
+            <Head>
+              <meta
+                content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                name="viewport"
+              />
+              <PageHead />
+            </Head>
+            <CreateViewContextProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </CreateViewContextProvider>
+          </ChakraProvider>
+        </PersistGate>
+      </Provider>
+    </Suspense>
   );
 };
 
